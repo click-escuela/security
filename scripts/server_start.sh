1ebae44b1b5337351293efff09e5f3b35620e744
clickescuela <83097465+clickescuela@users.noreply.github.com>
2021-08-29 18:24:21 -0300

    Update server_start.sh

--- scripts/server_start.sh
+++ scripts/server_start.sh
@@ -5,4 +5,5 @@ sudo rm -rf /home/ec2-user/server/security/security-service.pid
 
 echo"eliminando archivo"
 
-sudo java -jar security-0.0.1-SNAPSHOT.jar > /dev/null 2> /dev/null < /dev/null & echo $! > security-service.pid
+sudo java -jar -Dspring.datasource.url=jdbc:mysql://clickescuela.ccmmeszml0xl.us-east-2.rds.amazonaws.com:3306/clickescuela -Dspring.datasource.username=root -Dspring.datasource.password=secret123 \
+security-0.0.1-SNAPSHOT.jar > /dev/null 2> /dev/null < /dev/null & echo $! > security-service.pid
