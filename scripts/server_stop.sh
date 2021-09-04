3e352075f24f142520a5f47aa93d44914316730d
Evelin Aragon <eve_aragon@hotmail.com>
2021-09-04 20:30:21 -0300

    fix logs

--- scripts/server_stop.sh
+++ scripts/server_stop.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+chmod +x /home/ec2-user/server/security/logs
+chmod +x /home/ec2-user/server/security/logs/error.log
+chmod +x /home/ec2-user/server/security/logs/debug.log
+
 var="$(cat /home/ec2-user/server/security/security-service.pid)"
 sudo kill $var
 sudo rm -rf /home/ec2-user/server/security/security-service.pid
