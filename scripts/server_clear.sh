72300c61ec3a997a625923fe2b5a4d8e63118d96
Evelin <eve_aragon@hotmail.com>
2021-10-12 16:21:52 -0300

    subir a produccion

--- scripts/server_clear.sh
+++ scripts/server_clear.sh
@@ -1,2 +1,8 @@
 #!/usr/bin/env bash
-sudo rm -rf /home/ec2-user/server/security
+sudo rm -rf /home/ec2-user/server/security/appspec.yml
+sudo rm -rf /home/ec2-user/server/security/security-0.0.1-SNAPSHOT.jar
+sudo rm -rf /home/ec2-user/server/security/gateway-service.pid
+sudo rm -rf /home/ec2-user/server/security/logs
+sudo rm -rf /home/ec2-user/server/security/server_stop.sh
+sudo rm -rf /home/ec2-user/server/security/fix_provileges.sh
+sudo rm -rf /home/ec2-user/server/security/server_clear.sh
---
+++ scripts/server_prestart.sh
@@ -0,0 +1,4 @@
+
+#!/usr/bin/env bash
+cd /home/ec2-user/server/security
+sudo sh server_start.sh
