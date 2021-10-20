a180c05c304421d3dde191a9c06e6253f24f4d24
Evelin <eve_aragon@hotmail.com>
2021-10-19 22:25:15 -0300

    subir a dev

--- scripts/server_clear.sh
+++ scripts/server_clear.sh
@@ -1,2 +1,9 @@
 #!/usr/bin/env bash
-sudo rm -rf /home/ec2-user/server/security
+sudo rm -rf /home/ec2-user/server/security/appspec.yml
+sudo rm -rf /home/ec2-user/server/security/security-0.0.1-SNAPSHOT.jar
+sudo rm -rf /home/ec2-user/server/security/security-service.pid
+sudo rm -rf /home/ec2-user/server/security/logs
+sudo rm -rf /home/ec2-user/server/security/server_stop.sh
+sudo rm -rf /home/ec2-user/server/security/fix_privileges.sh
+sudo rm -rf /home/ec2-user/server/security/server_prestart.sh
+sudo rm -rf /home/ec2-user/server/security/server_clear.sh
