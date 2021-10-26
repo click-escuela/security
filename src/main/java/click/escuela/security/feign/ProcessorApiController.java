4fe4a8e9098199401c77c2213397aa7a8e2ea758
Tony-L-93 <tonyliendro@gmail.com>
2021-10-26 08:51:26 -0300

    Feign service was created in order to connect Security with Processor API

---
+++ src/main/java/click/escuela/security/feign/ProcessorApiController.java
@@ -0,0 +1,20 @@
+package click.escuela.security.feign;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+
+@FeignClient(name = "processor")
+public interface ProcessorApiController {
+
+	// EmailController
+	@PostMapping(value = "/school/{schoolId}/email")
+	public String automaticCreation(@RequestParam(value = "password") String password,
+			@RequestParam("userName") String userName, @RequestParam("email") String email,
+			@PathVariable("schoolId") String schoolId);
+
+
+}
